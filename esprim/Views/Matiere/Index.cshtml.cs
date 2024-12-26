70da610ec3757ba0e18b6e7e217e164ba6d85245
Oussama Fekih Hassen <hssanoussama@gmail.com>
2024-12-26 22:36:11 +0100

    add Views

---
+++ esprim/Views/Matiere/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace esprim.Views.Matiere
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
